c231e499734c426566ba528c6e0ea5a44d005e9a
bogdanchayka <bogdanchayka@outlook.com>
2022-07-16 21:07:52 +0200

    first commit

---
+++ webserwer.ts
@@ -0,0 +1,41 @@
+import { serve } from "https://deno.land/std@0.148.0/http/server.ts";
+import { parseHTML } from "https://esm.sh/linkedom";
+
+const port = 8080;
+const URL = "https://elvationusa.com"
+const textResponse = await fetch(URL);
+const textData = await textResponse.text();
+
+const appendScriptsToBody = (HTMLString: string) => {
+  const { document } = parseHTML(HTMLString);
+
+  const scriptTags = document.getElementsByTagName("script");
+  
+  // Remove all script tags from head
+  scriptTags.forEach(element => {
+    element.remove()
+  });
+  
+  // Add all script tags before </body> instead
+  scriptTags.forEach(element => {
+    document.body.appendChild(element)
+  });
+
+  return document.toString()
+}
+
+const output = appendScriptsToBody(textData)
+
+const handler = (request: Request): Response => {
+  return new Response(
+    output,
+    {
+      headers: {
+        "content-type": "text/html; charset=utf-8",
+      },
+    },
+  );
+};
+
+console.log(`HTTP webserver running. Access it at: http://localhost:8080/`);
+await serve(handler, { port });
